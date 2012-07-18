1138fd6dc79e1fc883bea8a63fb9c6c85122b022
fishsauce <fishsauce.08@gmail.com>
2012-07-18 08:58:40 +0700

    run

--- public/javascripts/testcase.js
+++ public/javascripts/testcase.js
@@ -174,6 +174,7 @@ var TestCase = Spine.Controller.sub({
 	getDetails : function() {
 		if ($("#testcase_" + this.id + " .endpoints .endpoint").size() == 0) {
 			var controller = this;
+			console.log("/api_in_testcase/" + this.id);
 			$.get("/api_in_testcase/" + this.id, null, function(res) {
 				controller.endpoints.empty();
 				controller.endpoints.append(res);
